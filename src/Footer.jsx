cc4e73e5535c7dd61060b5b4fec6df302f7888fc
Farhad-Hossain <farhad.cse@yahoo.com>
2024-04-18 08:41:27 +0600

    initial

---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>@copy; {new Date().getFullYear()} Your Website Name</p>
+        </footer>
+    )
+}
+export default Footer;
